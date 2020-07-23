20e308730c8c874132a5efae9d90075b00dea293
Justin Moon <mail@justinmoon.com>
2020-07-23 11:18:27 -0500

    Print unix time every second

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use std::time::SystemTime;
+use std::thread::sleep;
+use std::time::{Duration, SystemTime};
 
 fn main() {
-    let unix_time = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap();
-    println!("{}", unix_time.as_secs());
+    loop {
+        // Print current time
+        let unix_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        println!("{}", unix_time.as_secs());
+
+        // Sleep one second
+        sleep(Duration::from_secs(1));
+    }
 }
